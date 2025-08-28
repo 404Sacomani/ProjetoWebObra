ba4f8f77d24dfc7cbfed4b240d2508ecdbf9410c
Marcos Sacomani <71357829+404Sacomani@users.noreply.github.com>
2025-08-28 08:46:12 -0300

    Atualizar o HomePage.jsx

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -15,7 +15,7 @@ function HomePage() {
   useEffect(() => {
     async function fetchArtigos() {
       try {
-        const response = await fetch('http://localhost:3000');
+        const response = await fetch('https://projeto-obra-ativa-3koc4.ondigitalocean.app/');
         if (!response.ok) {
           throw new Error('Falha ao carregar artigos.');
         }

0f4d37798558949536255921071d9c91caad692e
Marcos <marcosrodrigues8922@gmail.com>
2025-09-01 22:14:33 -0300

    pequenas implementações 2

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     //baseURL: 'https://walrus-app-y2l9m.ondigitalocean.app/'
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://walrus-app-y2l9m.ondigitalocean.app'
 })
 
 export default api
